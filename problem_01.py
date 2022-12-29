be0fd23289c15a7d8cec43bd0c0a67329c2e1b41
mahfuz502 <mahfuzk502@gmail.com>
2022-12-29 15:09:39 +0600

    done

--- problem_01.py
+++ problem_01.py
@@ -8,3 +8,6 @@
 # Twinkle, twinkle, little star,
 # 	How I wonder what you are
 
+x = "Twinkle, twinkle, little star,\n \t \t  How I wonder what you are!\n \t \t \t \t Up above the world so high,\n \t \t \t \t" \
+    " Like a diamond in the sky.\n Twinkle, twinkle, little star,\n \t \t How I wonder what you are\n"
+print(x)

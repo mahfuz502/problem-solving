bec5b123237f18f79aec7354decae9ddc40fccec
mahfuz502 <mahfuzk502@gmail.com>
2022-12-29 21:50:46 +0600

    done

--- problem_02.py
+++ problem_02.py
@@ -1,6 +1,7 @@
 # 2. Write a Python program to get the Python version you are using
-# py or python version --
+import sys
+print(sys.version)
+print(sys.version_info)
 
-x = "Twinkle, twinkle, little star,\n \t \t  How I wonder what you are!\n \t \t \t \t Up above the world so high,\n \t \t \t \t" \
-    " Like a diamond in the sky.\n Twinkle, twinkle, little star,\n \t \t How I wonder what you are\n"
-print(x)
+
+
